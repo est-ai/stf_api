a853346667c7a8976d917a0261064665c6718e8f
snowyunee <snowyunee@estsoft.com>
2022-04-22 15:21:39 +0900

    update inference.py

--- inference.py
+++ inference.py
@@ -4,9 +4,10 @@ from glob import glob
 import argparse
 import os
 import time
+import sys
 
 
-def main(args):
+def main_(args):
     s1 = time.time()
 
     stf.preprocess_template(config_path=args.config_path,
@@ -16,23 +17,25 @@ def main(args):
                             callback=None,
                             device=args.device,
                             verbose=args.verbose)
-    print('1 preprocess_template ')
+    if args.verbose:
+        print('1 preprocess_template ')
 
     model = stf.create_model(config_path=args.config_path,
                              checkpoint_path=args.checkpoint_path,
                              work_root_path=args.work_root_path,
                              device=args.device,
                              verbose=args.verbose)
-    print('2 create_model ')
+    if args.verbose:
+        print('2 create_model ')
 
     template = stf.template_video(model=model,
                                   template_video_path=args.template_video_path,
                                   callback=None,
                                   verbose=args.verbose)
-    print('3 template_video ')
+    if args.verbose:
+        print('3 template_video ')
     s2 = time.time()
     
-    os.makedirs(os.path.dirname(args.output), exist_ok=True)
     template.gen4(wav_path=args.wav_path,
                  wav_std=True,
                  wav_std_ref_wav=args.wav_std_ref_wav,
@@ -41,10 +44,23 @@ def main(args):
                  head_only=False,
                  out_path=args.output,
                  )
-    print('#### ', time.time() - s1)
-    print('#### ', time.time() - s2)
-    print('4 gen ')
+    if args.verbose:
+        print('#### total tile: ', time.time() - s1)
+        print('#### gen time: ', time.time() - s2)
+        print('4 gen ')
+    return 1
     
+
+def main(args):
+    try:
+        r = main_(args)
+        print('end~!! ', r)
+    except:
+        print('exception~!!!!')
+        sys.exit(0)
+        quit()
+    sys.exit(r)
+
     
 if __name__ == '__main__':
     
@@ -62,26 +78,27 @@ if __name__ == '__main__':
     
     parser.add_argument('--verbose',  action='store_true')
     args = parser.parse_args()
-    print(args)
+    #print(args)
 
     # parameter 확인
     if not os.path.exists(args.wav_path):
         print('file is not exist:', args.wav_path)
-        exit()
+        sys.exit(0)
     if not os.path.exists(args.template_video_path):
         print('file is not exist:', args.template_video_path)
-        exit()
+        sys.exit(0)
     if not os.path.exists(args.config_path):
         print('file is not exist:', args.config_path)
-        exit()
+        sys.exit(0)
     if not os.path.exists(args.checkpoint_path):
         print('file is not exist:', args.checkpoint_path)
-        exit()
+        sys.exit(0)
     if not os.path.exists(args.wav_std_ref_wav):
         print('file is not exist:', args.wav_std_ref_wav)
-        exit()
+        sys.exit(0)
     if not os.path.exists(args.reference_face):
         print('file is not exist:', args.reference_face)
-        exit()
+        sys.exit(0)
 
     main(args)
+
